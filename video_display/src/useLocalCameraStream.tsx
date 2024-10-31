cab2bcedc4ad40c7965a6631e4dd610b779a9beb
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-10-31 16:58:22 +0100

    Feat:Capturing the camera+Creating video chat rooms

---
+++ video_display/src/useLocalCameraStream.tsx
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+
+export function useLocalCamerStream(){
+    const [localStream,setLocalStream]=useState<MediaStream | null>(null);
+    useEffect(()=>
+    {
+        navigator.mediaDevices
+        .getUserMedia({video:true,audio:true})
+        .then((stream)=>{
+            setLocalStream(stream);
+        })
+    },[]);
+    return{
+        localStream,
+    };
+}
